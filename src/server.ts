ecc443af630b500d292c5e0d524663d2a68c0998
YasmimRoza <yasmim.souza@cits.br>
2022-04-14 11:09:20 -0400

    feito separacao da bibliotecas

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,5 @@
 import express from 'express'
 
 const server = express()
-// first
 
-const createCsvWriter = require('csv-writer').createObjectCsvWriter
-const csvWriter = createCsvWriter({
-  path: 'teste.csv',
-  header: [
-    {id: 'name', title: 'Name'},
-    {id: 'surname', title: 'Surname'},
-    {id: 'age', title: 'Age'},
-    {id: 'gender', title: 'Gender'},
-  ]
-})
-
-const data = [
-  {
-    name: 'Yasmim',
-    surname: 'Roza',
-    age: 23,
-    gender: 'F',
-  }
-];
-
-csvWriter
-  .writeRecords(data)
-  .then(()=> console.log('The CSV file was written successfully'));
-
-// last
 server.listen(3000, () => console.log('Hello World!!'))
